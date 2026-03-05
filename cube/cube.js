b8963ad56560c72300a90d48e7097e6fec82a812
Joseph Navin <josephfnavin@gmail.com>
2026-03-05 00:27:27 -0500

    cubelet based implementation

--- cube/cube.js
+++ cube/cube.js
@@ -1,5 +1,7 @@
 import HoriontalSectionCubeletCube from './horizontalSection/cube.js';
 import StandardLayoutArrayCube from './standardLayoutArray/cube.js';
+import unorderedCubelets from './unorderedCubelets/cube.js';
 
-export default StandardLayoutArrayCube;
+// export default StandardLayoutArrayCube;
 // export default HoriontalSectionCubeletCube;
+export default unorderedCubelets;
--- cube/cubeletValues.js
+++ cube/cubeletValues.js
@@ -1,5 +1,7 @@
 import StandardLayoutArrayCubeValues from './standardLayoutArray/solvedCube.js';
 import HoriontalSectionCubeletCubeValues from './horizontalSection/solvedCube.js'
+import unorderedCubeletsValues from './unorderedCubelets/solvedCube.js';
 
-export default StandardLayoutArrayCubeValues;
-//export default HoriontalSectionCubeletCubeValues;
+// export default StandardLayoutArrayCubeValues;
+// export default HoriontalSectionCubeletCubeValues;
+export default unorderedCubeletsValues;
